dd0a42c1a6ccf21ea500f8dbae629f53cee6b4d0
gin gu <bobo1314love@vip.qq.com>
2020-02-08 22:19:00 +0800

    Install Gitlab-CE shell script

---
+++ gitlab-ce/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Description: Install Gitlab-CE on CentOS7
+# Author: Gin Gu <bobo1314love@vip.qq.com>
+# Version: 1.0.0
+# Creation Time: 2020/02/08 22:06
+
+export PATH=~/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Generate Gitlab-CE repo
+cat > /etc/yum.repos.d/gitlab-ce.repo << EOF
+[gitlab-ce]
+name=Gitlab CE
+enabled=1
+baseurl=https://mirrors.tuna.tsinghua.edu.cn/gitlab-ce/yum/el7/
+gpgcheck=0
+EOF
+
+# Make cache
+yum makecache
+
+# Install Gitlab-CE
+yum install gitlab-ce -y
+
+# Configure and Start Gitlab
+gitlab-ctl reconfigure
